f02621bdf21e9bd8adb2df4247eac35b3a9e5309
lkamh <123456@gmail.com>
2024-02-23 10:08:13 +0800

    update

--- 0.js
+++ 0.js
@@ -255,8 +255,8 @@ function swipe_to_view_the_last_answer(answer_uis) {
 function click_answer_radio_button(answer_uis, question, answers, idx, isMustPost, obj_node) {
     answer_uis[idx].parent().click();
     var ansb = obj_node.child(1).bounds();
-    console.log(ansb);
     var answers_region = [ansb.left, ansb.top, ansb.width(), ansb.height()]
+    console.log(answers_region);
     sleep(266);
     if (textEndsWith(imagetext_true).exists()) {
         console.log("点击正确");
